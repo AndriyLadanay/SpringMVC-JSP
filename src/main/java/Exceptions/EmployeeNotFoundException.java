e5c4a731d8226c444c8dcb471debe8a8b5cc185c
AndriyLadanay <33180018+AndriyLadanay@users.noreply.github.com>
2018-04-23 12:44:33 +0300

    Delete EmployeeNotFoundException.java

--- src/main/java/Exceptions/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-
-public class EmployeeNotFoundException extends Exception {
-    public EmployeeNotFoundException(int id){
-        super("EmployeeNotFoundException with id="+id);
-    }
-}
